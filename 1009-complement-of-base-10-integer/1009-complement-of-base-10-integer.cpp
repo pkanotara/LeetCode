28a4e81d4eb65e4a017ec4a0bc6425c0f9620156
Pravin Kanotara <157233873+pkanotara@users.noreply.github.com>
2026-03-12 00:39:16 +0530

    Time: 0 ms (100%), Space: 7.8 MB (90.31%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,15 +1,17 @@
-
 class Solution {
 public:
     int bitwiseComplement(int n) {
+
         if(n == 0) return 1;
-        int res = 0;
-        long long power = 1;
-        while(n > 0){
-            if(n % 2 == 0) res += power;
-            power *= 2;
-            n /= 2;
+
+        int mask = 0;
+        int temp = n;
+
+        while(temp){
+            mask = (mask << 1) | 1;
+            temp >>= 1;
         }
-        return res;
+
+        return mask ^ n;
     }
 };
